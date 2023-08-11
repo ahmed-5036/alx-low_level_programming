f4fb84285c402a4c5ebdc9c42ed5d3426795dcf2
ahmed-5036 <ahmedofficial5036@gmail.com>
2023-08-11 07:10:47 -0700

    create printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+/**
+ * main - Entry point
+ *
+ * Description: This is the main function that uses the 'printf' function to
+ * print the text "with proper grammar, but the outcome is a piece of art,"
+ * followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
+
