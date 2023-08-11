9c06d2ff6f5362b4304b6256ab07cde1b93000e7
ahmed-5036 <ahmedofficial5036@gmail.com>
2023-08-11 07:00:43 -0700

    update puts betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
+/**
+ * main - Entry point
+ *
+ * Description: This function prints a specific text using the `puts` function.
+ *
+ * Return: Always 0 (Success)
+ */
+
 #include <stdio.h>
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
