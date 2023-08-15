277f55c37a69bc1a5e63ec46d7d38a724f93a069
ahmed-5036 <ahmedofficial5036@gmail.com>
2023-08-15 13:26:23 -0700

    created 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+for (int i = 'a'; i <= 'z'; i++)
+_putchar(i);
+_putchar('\n');
+}
