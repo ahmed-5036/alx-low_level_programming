b87c7017f7137962cb9bbe7ae0780c16a3300986
Ahmed maher <ahmedofficial5036@gmail.com>
2023-08-18 04:46:03 +0300

    Create 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * fizz_buzz - Prints the numbers from 1 to 100, with Fizz for multiples of 3,
+ *             Buzz for multiples of 5, and FizzBuzz for multiples of both 3 and 5.
+ */
+void fizz_buzz(void)
+{
+    int i;
+
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+printf("FizzBuzz");
+else if (i % 3 == 0)
+printf("Fizz");
+else if (i % 5 == 0)
+printf("Buzz");
+else
+printf("%d", i);
+if (i != 100)
+printf(" ");
+}
+printf("\n");
+}
