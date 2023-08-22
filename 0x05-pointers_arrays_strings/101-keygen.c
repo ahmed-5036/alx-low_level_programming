ad345e110c34b55b7e7eeb663d3e105a399af438
Ahmed maher <ahmedofficial5036@gmail.com>
2023-08-22 05:34:02 +0300

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,24 +2,23 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define PASSWORD_LENGTH 12
-
 int main(void)
 {
     srand(time(NULL));
 
-    char password[PASSWORD_LENGTH + 1];
+    char password[12 + 1];
     const char charset[] = "abcdefghijklmnopqrstuvwxyz0123456789";
+    int password_length = 12; // Length of password
 
     int i;
 
     /* Generate a password using sequential characters from charset */
-    for (i = 0; i < PASSWORD_LENGTH; i++)
+    for (i = 0; i < password_length; i++)
     {
         password[i] = charset[i % (sizeof(charset) - 1)];
     }
 
-    password[PASSWORD_LENGTH] = '\0';
+    password[password_length] = '\0';
 
     printf("%s\n", password);
 
