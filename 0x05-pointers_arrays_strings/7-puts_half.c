4fd86fd13f1f33de13950eb0d05156ed382dca06
Ahmed maher <ahmedofficial5036@gmail.com>
2023-08-22 05:17:45 +0300

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,9 +7,10 @@
 void puts_half(char *str)
 {
 int length = 0;
+int i;
 while (str[length] != '\0')
 length++;
-for (int i = (length + 1) / 2; i < length; i++)
+for (i = (length + 1) / 2; i < length; i++)
 _putchar(str[i]);
 _putchar('\n');
 }
