f61f99da9c370eb0cb8a7450edfc8ea696334f54
Ahmed maher <ahmedofficial5036@gmail.com>
2023-08-25 05:24:53 +0300

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,25 +1,33 @@
+#include "main.h"
+
 /**
- * leet - Encodes a string into "1337" text
- * @str: The input string
+ * leet - Encodes a string into 1337
+ * @s: Input string
  *
- * Return: Pointer to the modified string
+ * Return: Pointer to the resulting string
  */
-char *leet(char *str)
+char *leet(char *s)
 {
-char *ptr = str;
-char *leet_replace = "aAeEoOtTlL";
-char *leet_with = "4433007711";
-while (*ptr)
-{
-for (int i = 0; i < 10; i++)
-{
-if (*ptr == leet_replace[i])
-{
-*ptr = leet_with[i];
-break;
-}
-}
-ptr++;
-}
-return (str);
+    char *ptr = s;
+    char leetMap[256] = {0};
+
+    leetMap['a'] = '4';
+    leetMap['A'] = '4';
+    leetMap['e'] = '3';
+    leetMap['E'] = '3';
+    leetMap['o'] = '0';
+    leetMap['O'] = '0';
+    leetMap['t'] = '7';
+    leetMap['T'] = '7';
+    leetMap['l'] = '1';
+    leetMap['L'] = '1';
+
+    while (*ptr)
+    {
+        if (leetMap[*ptr])
+            *ptr = leetMap[*ptr];
+        ptr++;
+    }
+
+    return s;
 }
