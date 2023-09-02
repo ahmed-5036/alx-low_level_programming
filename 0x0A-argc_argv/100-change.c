003ec34f66d280bb8f226d224c99417dacb51432
ahmed-5036 <ahmedofficial5036@gmail.com>
2023-09-02 09:48:45 -0700

    update advanced task

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,23 +10,28 @@
  */
 int main(int argc, char *argv[])
 {
+int cents;
+int i;
+int coins[5] = {25, 10, 5, 2, 1};
+int num_coins;
+int count;
+
 if (argc != 2)
 {
 printf("Error\n");
 return (1);
 }
-int cents = atoi(argv[1]);
+cents = atoi(argv[1]);
 
 if (cents < 0)
 {
 printf("0\n");
 return (0);
 }
-int coins[] = {25, 10, 5, 2, 1};
-int num_coins = sizeof(coins) / sizeof(coins[0]);
-int count = 0;
+num_coins = sizeof(coins) / sizeof(coins[0]);
+count = 0;
 
-for (int i = 0; i < num_coins; i++)
+for (i = 0; i < num_coins; i++)
 {
 while (cents >= coins[i])
 {
