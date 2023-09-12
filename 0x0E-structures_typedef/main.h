64d401fbaa0a4a78cf8a030849d7bc9a217899ef
Ahmed maher <ahmedofficial5036@gmail.com>
2023-09-12 05:54:50 +0300

    Update main.h

--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -1,7 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Define a new type struct dog with the following elements */
+#include <stdio.h>
+
+/**
+ * struct dog - Defines a dog's characteristics
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ */
 struct dog
 {
     char *name;
@@ -9,20 +16,8 @@ struct dog
     char *owner;
 };
 
-/* Initialize a variable of type struct dog */
+/* Function prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
-/* Print a struct dog */
 void print_dog(struct dog *d);
 
-/* Define a new type dog_t as a new name for the type struct dog */
-typedef struct dog dog_t;
-
-/* Create a new dog */
-dog_t *new_dog(char *name, float age, char *owner);
-
-/* Free dogs */
-void free_dog(dog_t *d);
-
 #endif /* MAIN_H */
-
