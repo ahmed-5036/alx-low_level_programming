797729fa44b1048d0e5a89a1c4b6f7d2f19d2953
Ahmed maher <ahmedofficial5036@gmail.com>
2023-09-27 05:38:27 +0300

    Update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -15,7 +15,7 @@ size_t print_listint(const listint_t *h);
 
 /* Function to return the number of elements in a listint_t list */
 size_t listint_len(const listint_t *h);
-
+void free_listint2(listint_t **head);
 /* Function to add a new node at the beginning of a listint_t list */
 listint_t *add_nodeint(listint_t **head, const int n);
 
