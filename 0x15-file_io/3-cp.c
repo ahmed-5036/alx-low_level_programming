89c90dde6a5ae78a5f663489e14208f3e69e9d91
ahmed-5036 <ahmedofficial5036@gmail.com>
2023-10-03 19:45:05 -0700

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,65 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include "main.h"
 
 /**
- * main - Entry point
- * @ac: The number of command-line arguments
- * @av: An array of command-line argument strings
- *
- * Return: 0 on success, exit codes on failure
- */
-int main(int ac, char **av)
-{
-	int fd_from, fd_to, bytes_read, bytes_written;
-	char buffer[1024];
-
-	if (ac != 3)
-	{
-		dprintf(2, "Usage: %s file_from file_to\n", av[0]);
-		exit(97);
-	}
-
-	fd_from = open(av[1], O_RDONLY);
-	if (fd_from == -1)
-		handle_error("Error: Can't read from file", av[1], 98);
-
-	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
-		handle_error("Error: Can't write to", av[2], 99);
-
-	while ((bytes_read = read(fd_from, buffer, sizeof(buffer))) > 0)
-	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1)
-			handle_error("Error: Can't write to", av[2], 99);
-	}
-
-	if (bytes_read == -1)
-		handle_error("Error: Can't read from file", av[1], 98);
-
-	close_files(fd_from, fd_to);
-	return (0);
-}
-
-/**
- * handle_error - Handles and exits with an error message
- * @message: The error message
- * @file: The filename associated with the error
- * @exit_code: The exit code to use
+ * handle_error - function to print error messages and exit
+ * @message: the error message
+ * @file: the name of the file causing the error
+ * @exit_code: the exit code to use
  */
 void handle_error(const char *message, const char *file, int exit_code)
 {
-	dprintf(2, "%s %s\n", message, file);
-	exit(exit_code);
+dprintf(STDERR_FILENO, "%s %s\n", message, file);
+exit(exit_code);
 }
-
 /**
- * close_files - Closes two file descriptors and exits on failure
- * @fd1: The first file descriptor
- * @fd2: The second file descriptor
+ * close_files - function to close two file descriptors
+ * @fd1: the first file descriptor
+ * @fd2: the second file descriptor
  */
 void close_files(int fd1, int fd2)
 {
-	if (close(fd1) == -1 || close(fd2) == -1)
-		handle_error("Error: Can't close fd", "");
-}
+int result;
 
+result = close(fd1);
+if (result == -1)
+handle_error("Error: Can't close fd", "1", 100);
+result = close(fd2);
+if (result == -1)
+handle_error("Error: Can't close fd", "2", 100);
+}
+/**
+ * main - program entry point
+ * @argc: the argument count
+ * @argv: the argument values
+ * Return: 0 on success, various exit codes on failure
+ */
+int main(int argc, char *argv[])
+{
+int fd_from, fd_to;
+ssize_t bytes_read, bytes_written;
+char buffer[1024];
+
+if (argc != 3)
+handle_error("Usage: cp file_from file_to", "", 97);
+fd_from = open(argv[1], O_RDONLY);
+if (fd_from == -1)
+handle_error("Error: Can't read from file", argv[1], 98);
+fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+if (fd_to == -1)
+handle_error("Error: Can't write to", argv[2], 99);
+while ((bytes_read = read(fd_from, buffer, sizeof(buffer))) > 0)
+{
+bytes_written = write(fd_to, buffer, bytes_read);
+if (bytes_written == -1 || bytes_written != bytes_read)
+handle_error("Error: Can't write to", argv[2], 99);
+}
+if (bytes_read == -1)
+handle_error("Error: Can't read from file", argv[1], 98);
+close_files(fd_from, fd_to);
+return (0);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,6 +14,7 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int copy_file(const char *file_from, const char *file_to);
 void elf_header(const char *elf_filename);
-
+void handle_error(const char *message, const char *file, int exit_code);
+void close_files(int fd1, int fd2);
 #endif /* MAIN_H */
 
