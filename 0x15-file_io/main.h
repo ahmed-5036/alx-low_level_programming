4c735aa3a5aea76a491d0fb6104f064b6951bc16
ahmed-5036 <ahmedofficial5036@gmail.com>
2023-10-03 19:18:43 -0700

    task 0

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,14 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Include necessary headers here */
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 /* Function prototypes */
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
-/* Add other function prototypes here */
+int copy_file(const char *file_from, const char *file_to);
+void elf_header(const char *elf_filename);
 
 #endif /* MAIN_H */
 
